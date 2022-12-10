9e1372cd71c0c7d5d7b395ed3ebf6348b562b4f2
Alberth94 <108017350+Alberth94@users.noreply.github.com>
2022-12-10 16:16:07 +0100

    Update AddWords.js

--- src/components/AddWords.js
+++ src/components/AddWords.js
@@ -1,16 +1,16 @@
 import { useState } from 'react';
 import './AddWords.css';
 
-const AddWords = (props) => {
+const AddWords = ({wordToList, setWordToList}) => {
     const [word, setWord] = useState('');
     const [displayMessage, setDisplayMessage] = useState('');
 
-    const wordToList = () => {
-        if (!props.list.includes(word) && word !== "") {
-            props.list.push(word);
+    const wordInList = () => {
+        if (!wordToList.includes(word) && word !== "") {
+            setWordToList(wordToList.concat(word));
             setDisplayMessage('The word "' + word + '", was successfully added to the dictionary.')
             setWord('');
-        } else if (props.list.includes(word) && word !== "") {
+        } else if (wordToList.includes(word) && word !== "") {
             setDisplayMessage('The word "' + word + '", has already been added to the dictionary.')
         }
     };
@@ -19,7 +19,7 @@ const AddWords = (props) => {
         <div className='words-input'>
             <p>Add a word to the dictionary.</p>
             <input type='text' value={word} onChange={(e) => {setWord(e.target.value)}}/>
-            <button className='button1' type='submit' onClick={wordToList} >Submit</button>
+            <button className='button1' type='submit' onClick={wordInList} >Submit</button>
             <p>{displayMessage}</p>
         </div>
     );
